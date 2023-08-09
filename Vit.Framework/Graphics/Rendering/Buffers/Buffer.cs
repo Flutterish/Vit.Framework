1dc09f01b685aa84882d5268115a77c97d5f5058
Flutterish <CharmindYT@gmail.com>
2023-08-09 07:06:48 +0200

    remove unused files

--- Vit.Framework/Graphics/Rendering/Buffers/Buffer.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Vit.Framework.Graphics.Rendering.Buffers;
-
-//public abstract class Buffer<T, TBuffer> where T : unmanaged where TBuffer : INativeBuffer<T> {
-//	//protected TBuffer? NativeBuffer { get; private set; }
-//	//protected abstract TBuffer CreateNativeBuffer ( Renderer renderer );
-//}
--- Vit.Framework/Graphics/Rendering/Buffers/VertexBuffer.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Vit.Framework.Graphics.Rendering.Buffers;
-
-//public class VertexBuffer<T> : Buffer<T, INativeVertexBuffer<T>> where T : unmanaged {
-//	//protected override INativeVertexBuffer<T> CreateNativeBuffer ( Renderer renderer )
-//	//	=> renderer.CreateVertexBuffer<T>();
-//}
