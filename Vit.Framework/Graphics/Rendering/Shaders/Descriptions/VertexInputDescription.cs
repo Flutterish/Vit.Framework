3d9e4043933135f1de3cec8974f987b86717bfc9
Flutterish <CharmindYT@gmail.com>
2023-08-09 21:44:55 +0200

    make creating vertex input descriptions easier

--- Vit.Framework/Graphics/Rendering/Shaders/Descriptions/VertexInputDescription.cs
+++ Vit.Framework/Graphics/Rendering/Shaders/Descriptions/VertexInputDescription.cs
@@ -31,6 +31,14 @@ public record VertexBufferDescription {
 	public required BufferInputRate InputRate { get; init; }
 	public required ImmutableDictionary<uint, VertexAttributeDescription> AttributesByLocation { get; init; }
 
+	public static VertexBufferDescription Create ( uint stride, BufferInputRate inputRate, IEnumerable<(uint, VertexAttributeDescription)> attributes ) {
+		return new() {
+			Stride = stride,
+			InputRate = inputRate,
+			AttributesByLocation = attributes.ToImmutableDictionary( v => v.Item1, v => v.Item2 )
+		};
+	}
+
 	public override string ToString () {
 		return $"{{ Stride = {Stride}, InputRate = {InputRate}, Attributes = [\n\t{string.Join(",\n\t", AttributesByLocation.Select( 
 			x => $"[Location {x.Key}] = {x.Value}"
@@ -40,6 +48,12 @@ public record VertexBufferDescription {
 
 public record VertexInputDescription {
 	public required ImmutableDictionary<uint, VertexBufferDescription> BufferBindings { get; init; }
+
+	public static VertexInputDescription Create ( IEnumerable<(uint, VertexBufferDescription)> buffers ) {
+		return new() {
+			BufferBindings = buffers.ToImmutableDictionary( v => v.Item1, v => v.Item2 )
+		};
+	}
 
 	/// <summary>
 	/// Creates an input description for a shader containing one sequential vertex buffer.
@@ -96,6 +110,39 @@ public record VertexInputDescription {
 
 		return new() {
 			BufferBindings = buffers.ToImmutableDictionary()
+		};
+	}
+
+	/// <summary>
+	/// Creates an input description for a shader containing vertex buffers as described by the <c><paramref name="buffers"/></c> parameter.
+	/// </summary>
+	public static VertexInputDescription CreateGrouped ( ShaderInfo shader, params (BufferInputRate rate, uint[] attributes)[] buffers ) {
+		Dictionary<uint, VertexBufferDescription> resultBuffers = new();
+
+		uint bufferIndex = 0;
+		foreach ( var (rate, attributes) in buffers ) {
+			Dictionary<uint, VertexAttributeDescription> attributesResult = new();
+			uint offset = 0;
+			foreach ( var location in attributes ) {
+				var attrib = shader.Input.Resources.Single( x => x.Location == location );
+				var size = attrib.Type.PrimitiveType.SizeOf() * attrib.Type.FlattendedDimensions;
+				attributesResult.Add( location, new() {
+					DataType = attrib.Type,
+					Offset = offset
+				} );
+
+				offset += size;
+			}
+
+			resultBuffers.Add( bufferIndex++, new() { 
+				Stride = offset,
+				InputRate = rate,
+				AttributesByLocation = attributesResult.ToImmutableDictionary()
+			} );
+		}
+
+		return new() {
+			BufferBindings = resultBuffers.ToImmutableDictionary()
 		};
 	}
 
