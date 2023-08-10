e4e74e759d28310544f082c9706ad30090b5c5ce
Flutterish <CharmindYT@gmail.com>
2023-08-10 22:50:50 +0200

    make opengl buffer persistently mapped

--- Vit.Framework.Graphics.OpenGl/Buffers/Buffer.cs
+++ Vit.Framework.Graphics.OpenGl/Buffers/Buffer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.InteropServices;
 using Vit.Framework.Graphics.Rendering.Buffers;
-using Vit.Framework.Interop;
 using Vit.Framework.Memory;
 
 namespace Vit.Framework.Graphics.OpenGl.Buffers;
@@ -9,12 +8,13 @@ public interface IGlBuffer : IGlObject {
 	int Stride { get; }
 }
 
-public class Buffer<T> : DisposableObject, IGlBuffer, IDeviceBuffer<T>, IHostBuffer<T> where T : unmanaged {
-	public static readonly int Stride = Marshal.SizeOf( default(T) );
+public unsafe class Buffer<T> : DisposableObject, IGlBuffer, IDeviceBuffer<T>, IHostBuffer<T> where T : unmanaged {
+	public static readonly int Stride = Marshal.SizeOf( default(T) ); // TODO we still need to split this into host and device optimised buffers
 	int IGlBuffer.Stride => Stride;
 
 	public int Handle { get; }
 	public readonly BufferTarget Type;
+	public T* Data;
 	public Buffer ( BufferTarget type ) {
 		Type = type;
 		Handle = GL.GenBuffer();
@@ -23,17 +23,13 @@ public class Buffer<T> : DisposableObject, IGlBuffer, IDeviceBuffer<T>, IHostBuf
 	public void Allocate ( uint size, BufferUsage usageHint ) {
 		GL.BindBuffer( Type, Handle );
 
-		var offset = usageHint.HasFlag( BufferUsage.GpuPerFrame ) ? 0 : usageHint.HasFlag( BufferUsage.GpuRarely ) ? 6 : 3;
-		GL.BufferData( Type, (int)size * Stride, (nint)null, 
-			usageHint.HasFlag( BufferUsage.CpuRead ) ? (BufferUsageHint.StreamRead + offset) :
-			usageHint.HasFlag( BufferUsage.CpuWrite ) ? (BufferUsageHint.StreamDraw + offset) :
-			(BufferUsageHint.StreamCopy + offset)
-		);
+		var length = (int)size * Stride;
+		GL.BufferStorage( Type, length, (nint)null, BufferStorageFlags.MapWriteBit | BufferStorageFlags.MapPersistentBit | BufferStorageFlags.MapCoherentBit );
+		Data = (T*)GL.MapBufferRange( Type, 0, length, BufferAccessMask.MapWriteBit | BufferAccessMask.MapPersistentBit | BufferAccessMask.MapCoherentBit );
 	}
 
 	public unsafe void Upload ( ReadOnlySpan<T> data, uint offset = 0 ) {
-		GL.BindBuffer( Type, Handle );
-		GL.BufferSubData( Type, (int)offset, data.Length * Stride, (nint)data.Data() );
+		data.CopyTo( new Span<T>( (void*)this.Data, data.Length + (int)offset ).Slice( (int)offset ) );
 	}
 
 	protected override void Dispose ( bool disposing ) {
