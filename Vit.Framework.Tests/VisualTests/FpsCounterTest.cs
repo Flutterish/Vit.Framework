43e45ee48893b628d49c713caa3addf040044db8
Flutterish <CharmindYT@gmail.com>
2023-08-12 07:36:26 +0200

    update fps counter test
    decoupling it from app threads

--- Vit.Framework.Tests/VisualTests/FpsCounterTest.cs
+++ Vit.Framework.Tests/VisualTests/FpsCounterTest.cs
@@ -1,12 +1,29 @@
-﻿using Vit.Framework.TwoD.Layout;
+﻿using Vit.Framework.DependencyInjection;
+using Vit.Framework.Performance;
+using Vit.Framework.TwoD.Layout;
 using Vit.Framework.TwoD.UI.Components;
 
 namespace Vit.Framework.Tests.VisualTests;
 
 public class FpsCounterTest : TestScene {
+	UpdateCounter counter = new();
+
 	public FpsCounterTest () {
 		AddChild( new FpsCounter(), new() {
 			Size = new( 1f.Relative() )
 		} );
+	}
+
+	protected override IReadOnlyDependencyCache CreateDependencies ( IReadOnlyDependencyCache parent ) {
+		var cache = new DependencyCache( parent );
+		cache.Cache( new FpsCounter.FpsCounterData(
+			(" Updates Per Second", counter)
+		) );
+		return cache;
+	}
+
+	public override void Update () {
+		counter.Update();
+		base.Update();
 	}
 }
