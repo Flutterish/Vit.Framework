838fd4e9cb0f67e4c5d2a75c089713b33b364068
Flutterish <CharmindYT@gmail.com>
2023-08-27 22:10:03 +0200

    remove sample test scenes

--- Vit.Framework.Tests/Program.cs
+++ Vit.Framework.Tests/Program.cs
@@ -6,6 +6,7 @@ using Vit.Framework.Graphics.Vulkan;
 using Vit.Framework.Platform;
 using Vit.Framework.Tests.AudioApis;
 using Vit.Framework.Tests.GraphicsApis;
+using Vit.Framework.Tests.VisualTests;
 using Vit.Framework.Threading;
 using Vit.Framework.Windowing;
 using Vit.Framework.Windowing.Console;
@@ -17,8 +18,8 @@ public partial class Program : App {
 	public Program () : base( "Test App" ) { }
 
 	public static void Main () {
-		//var app = new TwoDTestApp( typeof( VisualTestRunner ) );
-		var app = new Program();
+		var app = new TwoDTestApp( typeof( VisualTestRunner ) );
+		//var app = new Program();
 		app.ThreadRunner.ThreadingMode = ThreadingMode.Multithreaded;
 		app.Run();
 		app = null;
